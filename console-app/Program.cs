ad057b02af75bc1e102824b854a520867cbd0ce2
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-12 17:50:53 +0330

    first commit

---
+++ console-app/Program.cs
@@ -0,0 +1,35 @@
+﻿using library;
+
+var studentsFileReader = new FileReader() {path = @"../files/students.txt"};
+var scoresFileReader = new FileReader() {path = @"../files/scores.txt"};
+
+string students = studentsFileReader.ReadFile();
+string scores = scoresFileReader.ReadFile();
+
+var studentsArr = Deserializer<Student>.DeserializeArrayOfJsonObjects(students);
+var scoresArr = Deserializer<Score>.DeserializeArrayOfJsonObjects(scores);
+
+var dataOfFirstThreeStudents = new Data[3];
+
+for (int i = 0; i < 3; i++){
+    var data = new Data(){
+    firstName = studentsArr[i].firstName,
+    lastName = studentsArr[i].lastName,
+    studentNumber = studentsArr[i].studentNumber
+    };
+    dataOfFirstThreeStudents[i] = data;
+}
+
+foreach (var score in scoresArr){
+    foreach(var data in dataOfFirstThreeStudents){
+        if (score.studentNumber == data.studentNumber){
+            data.AddScore(score.score);
+        }
+    }
+}
+
+foreach (var data in dataOfFirstThreeStudents){
+    Console.WriteLine(data.average);
+}
+
+
---
+++ library/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace library;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+public class FileReader
+{
+    public string path {set; get;}
+    public string ReadFile() => System.IO.File.ReadAllText(path);
+    
+}
+
+public class Student
+{
+    public string firstName{set; get;}
+    public string lastName{set; get;}
+    public int studentNumber{set; get;}
+}
+
+public class Score
+{
+    public float score {set; get;}
+    public int studentNumber {set; get;}
+    public string lesson{set; get;}
+}
+
+public static class Deserializer<T>
+{
+    public static List<T> DeserializeArrayOfJsonObjects(string jsonString) => 
+        JsonConvert.DeserializeObject<List<T>>(jsonString);
+    
+}
+
+public class Data{
+    public int studentNumber{set; get;}
+    public int count{set; get;} = 0;
+    public string firstName{set; get;}
+    public string lastName{set; get;}
+    public float average{set; get;}
+    public void AddScore(float newScore){
+        average = ((average * count) + newScore) / ++count;
+    }
+}
