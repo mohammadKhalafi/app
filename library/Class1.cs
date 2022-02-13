c1ba1b951a29b370434a2e516af4be6d9011d6a8
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-13 09:44:59 +0330

    refactoring

--- library/Class1.cs
+++ library/Class1.cs
@@ -36,3 +36,62 @@ public class Data{
         average = ((average * count) + newScore) / ++count;
     }
 }
+
+public class controller{
+
+    private List<Student> students;
+    private List<Score> scores;
+    private Data[] dataOfFirstThreeStudents;
+
+    public void Run(){
+        InitializeScores();
+        InitializeStudendts();
+        InitializeData();
+        CalcuolateAverage();
+        PrintAnswer();
+    }
+    private void InitializeStudendts(){
+        var studentsFileReader = new FileReader() {path = @"../files/students.txt"};
+        var studentsJsonData = studentsFileReader.ReadFile();
+        students = Deserializer<Student>.DeserializeArrayOfJsonObjects
+            (studentsJsonData);
+    }
+
+    private void InitializeScores(){
+        var scoresFileReader = new FileReader() {path = @"../files/scores.txt"};
+        var scoresJsonData = scoresFileReader.ReadFile();
+        scores = Deserializer<Score>.DeserializeArrayOfJsonObjects(scoresJsonData);
+    }
+
+    private void InitializeData(){
+        var data = new Data[3];
+        for (int i = 0; i < 3; i++){
+            var eachData = new Data(){
+                firstName = students[i].firstName,
+                lastName = students[i].lastName,
+                studentNumber = students[i].studentNumber
+            };
+            data[i] = eachData;
+        }
+        dataOfFirstThreeStudents = data;
+    }
+
+    private void CalcuolateAverage(){
+        foreach (var score in scores){
+            foreach(var eachData in dataOfFirstThreeStudents){
+                if (score.studentNumber == eachData.studentNumber){
+                    eachData.AddScore(score.score);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void PrintAnswer(){
+        foreach (var eachData in dataOfFirstThreeStudents){
+            Console.WriteLine(eachData.firstName + " " +
+                eachData.lastName + " : " + eachData.average);
+        }
+    }
+}
+
